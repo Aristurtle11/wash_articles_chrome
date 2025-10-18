fc8cd10e40bd6f360d8ac03f9da2df378c095d68
DengZhichao <mryellowpup@gmail.com>
2025-10-18 23:43:35 +0800

    整理工程原始遗留代码

--- src/shared/text.js
+++
@@ -1,8 +0,0 @@
-export function escapeHtml(input) {
-  return String(input ?? "")
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
-}
